eb4415f30fb387b90739379527cb4fc8abdd462c
deepak-kumar-70 <141209444+deepak-kumar-70@users.noreply.github.com>
2025-06-27 16:09:56 +0530

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -41,7 +41,7 @@ const App = () => {
       localStream.current = stream;
 
       if (localVideo.current) {
-        console.log('hello')
+        console.log('hello kk')
         localVideo.current.srcObject = stream;
         
         localVideo.current.muted = true;
